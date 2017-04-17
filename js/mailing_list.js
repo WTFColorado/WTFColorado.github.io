860a1bf7a5af7ae0930667c224fd34f048515acd
stormyscene <stormyscene@users.noreply.github.com>
2017-04-16 20:04:10 -0600

    Add styled.html

---
+++ js/mailing_list.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    $("input,select").jqBootstrapValidation({
+        preventSubmit: true
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
